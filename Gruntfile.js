a604eccbd6c13e912e56d4e9f80001a235e06290
Yaroslav <ya.taranenko@nmotors.kz>
2017-10-24 16:31:24 +0600

    without passport and mysql

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		express:{
+			dev: {
+				options:{
+					script: './bin/www'
+				}
+			}
+		},
+		watch:{
+			options:{
+				livereload: true
+			},
+			express:{
+				files: ['./app.js', './server/routes/*.js'],
+				tasks:['express:dev'],
+				options:{
+					spawn: false
+				}
+			},
+			public:{
+				files: ['./server/views/*.jade', './public/stylesheets/style.css']
+			}
+		}
+		
+	});
+
+	grunt.loadNpmTasks('grunt-express-server');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['express:dev', 'watch']);
+
+	grunt.registerTask('jshint', ['jshint']);
+};
